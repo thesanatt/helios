90db67bd81ea09510982027f52723d100f528a50
Sanat Gupta <sanat@Sanats-MacBook-Pro.local>
2026-03-26 19:13:34 -0400

    Fix CORS for production deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,8 +50,8 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origin_list,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
